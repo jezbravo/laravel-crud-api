48f411de3da6a0b4373b2fa99cbc71a7b3b98ffb
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-05-09 09:49:58 -0300

    Create basic crud structure

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,33 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Obtain all
+Route::get("/students", function () {
+    return "Students List";
+});
+
+// Obtain any
+Route::get("/students/{id}", function () {
+    return "Student";
+});
+
+// Create
+Route::post("/students", function () {
+    return "Student Creation";
+});
+
+// Update
+Route::patch("/students/{id}", function () {
+    return "Student Update";
+});
+
+// Delete
+Route::delete("/students/{id}", function () {
+    return "Student Delete";
+});
